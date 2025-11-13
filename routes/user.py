9c8968f7ee6467c0db8b3c9e748a754f3e0c5d78
kelvin-langat <kelvin.ki.langat@gmail.com>
2025-11-13 10:00:21 +0300

    In routes making a root dir /user

--- routes/user.py
+++ routes/user.py
@@ -8,17 +8,21 @@ from schemas.user import (FullUserProfile,
 from services.user import UserService
 
 def create_user_router() -> APIRouter:
-    user_router = APIRouter()
+    user_router = APIRouter(
+        prefix="/user_endpoint",
+        tags =["user"]
+    )
     user_service = UserService()
+    
+    
+    @user_router.get("/all", response_model=MultipleUsersResponse)
+    def get_all_users_paginated(start: int = 0, limit: int = 2):
+        users, total = user_service.get_all_users_with_pagination(start, limit)
+        formatted_users = MultipleUsersResponse(users=users, total=total)
+        return formatted_users
 
 
-    @user_router.get("/user/me", response_model=FullUserProfile)
-    def test_endpoint():
-        full_user_profile = user_service.getUserInfo()
-        return full_user_profile
-
-
-    @user_router.get("/user/{user_id}", response_model=FullUserProfile)
+    @user_router.get("/{user_id}", response_model=FullUserProfile)
     def get_user_by_id(user_id: int):
         """
         Endpoint for retrieving a FullUserProfile by the user's unique indentifier
@@ -28,24 +32,17 @@ def create_user_router() -> APIRouter:
         full_user_profile = user_service.getUserInfo(user_id)
         return full_user_profile
 
-    @user_router.put("/user/user_id")
+    @user_router.put("/user_id")
     def update_user(user_id: int, full_profile_info: FullUserProfile):
         user_service.create_update_user(full_profile_info, user_id)
         return None
 
-    @user_router.delete("/user/user_id")
+    @user_router.delete("/user_id")
     def remove_user(user_id, int):
         user_service.delete_user(user_id)
 
 
-    @user_router.get("/user", response_model=MultipleUsersResponse)
-    def get_all_users_paginated(start: int = 0, limit: int = 2):
-        users, total = user_service.get_all_users_with_pagination(start, limit)
-        formatted_users = MultipleUsersResponse(users=users, total=total)
-        return formatted_users
-
-
-    @user_router.post("/users", response_model=createUserResponse)
+    @user_router.post("/", response_model=createUserResponse)
     def add_user(full_profile_info: FullUserProfile):
         user_id = user_service.create_update_user(full_profile_info)
         print("doc string of create_update_user:\n", user_service.create_update_user.__doc__)
